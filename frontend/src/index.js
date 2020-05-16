286b93ad066c5f0d8f6743c48b7707973f0e586a
邓鑫波 <mymail33333@163.com>
2020-05-16 18:32:19 +0800

    更新

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Router from './router';
+
+import './style/index.less';
 
 ReactDOM.render(
   <React.StrictMode>
--- frontend/src/layout/index.jsx
+++ frontend/src/layout/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Layout } from 'antd'
+import './index.less'
 
+const { Header, Content } = Layout
 
-
-export default function BasicLayout() {
+export default function BasicLayout({children}) {
     return (
-        <Layout>
-            
+        <Layout className='layout'>
+            <Header className='layout-header'>
+                <div className='layout-header-title'>
+                    新冠疫情模拟预测与多尺度舆情监测分析系统
+                </div>
+            </Header>
+            <Content>
+                {children}
+            </Content>
         </Layout>
     )
 }
