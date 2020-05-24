5d7d10a61bde0973eca145c7286870f7d7f226fc
邓鑫波 <mymail33333@163.com>
2020-05-24 17:22:16 +0800

    修复代理配置文件错误

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require("http-proxy-middleware");
- 
+const {createProxyMiddleware} = require("http-proxy-middleware");
+
 module.exports = function(app) {
   app.use(
-    proxy("/d/**", {
+    createProxyMiddleware("/d/", {
       target: "http://112.74.89.57:8000/",
       changeOrigin: true,
       pathRewrite: {
