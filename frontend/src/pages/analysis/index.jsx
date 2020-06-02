7b58ef364f809c6a566c1923ef717ca1335b4a89
relyon <1010548471@qq.com>
2020-06-02 20:54:17 +0800

    新增疫情分析页面

--- frontend/src/pages/analysis/index.jsx
+++ frontend/src/pages/analysis/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Row, Col } from 'antd'
+import Map from './components/map'
 
 export default function Opinion() {
     return (
         <Row gutter={16}>
-            <Col span={8}>
+            <Col span={6}>
 
             </Col>
-            <Col span={8} style={{height: '300px'}}>地图
+            <Col span={12}>
+                <Map />
             </Col>
-            <Col span={8}>
+            <Col span={6}>
 
             </Col>
 
