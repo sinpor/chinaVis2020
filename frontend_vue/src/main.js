63f291ab8889b558ff6335761b72d7073aa057cf
邓鑫波 <mymail33333@163.com>
2020-06-08 22:48:29 +0800

    加载全局组件

--- frontend_vue/src/main.js
+++ frontend_vue/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import dataV from '@jiaminghi/data-view'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+Vue.use(dataV)
 
 Vue.config.productionTip = false
 
