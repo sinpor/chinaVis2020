15e76a2dce5edda6e846e5ffac387644d410abd8
邓鑫波 <mymail33333@163.com>
2020-06-09 01:29:39 +0800

    全局组件

---
+++ frontend_vue/src/components/section/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="section">
+        <div class="section-header">{{title}}</div>
+        <div class="section-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'section',
+    props: ['title']
+}
+</script>
+
+<style lang="less" scoped>
+    .section {
+        .section {
+            height: 20px;
+        }
+    }
+</style>
