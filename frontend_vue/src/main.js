b2e54e0b1fe546c775d834b2dcd02307dd590dfa
邓鑫波 <mymail33333@163.com>
2020-06-08 23:21:56 +0800

    增加样式

--- frontend_vue/src/main.js
+++ frontend_vue/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import dataV from '@jiaminghi/data-view'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './assets/style/index.less'
 
 Vue.use(ElementUI)
 Vue.use(dataV)
