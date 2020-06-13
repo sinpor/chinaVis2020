b4e4fcffc33ffad6dc9b14f3cbacdb77c6ffd635
邓鑫波 <mymail33333@163.com>
2020-06-13 20:05:11 +0800

    更新社交网络

--- frontend/src/pages/network/components/info/index.jsx
+++ frontend/src/pages/network/components/info/index.jsx
@@ -2,38 +2,93 @@ import React from "react"
 import store from "../../store"
 import { observer } from "mobx-react"
 import Section from "@/components/section"
+import { List, Space, Avatar, Collapse } from "antd"
+import {
+	MessageOutlined,
+	LikeOutlined,
+	UserOutlined,
+	ShareAltOutlined,
+} from "@ant-design/icons"
 
-const TRANSLATE = {
-	"﻿weiboname": "用户名",
-	weibotext: "微博",
-	topic: "话题",
-	date: "日期",
-	time: "时间",
-	forward: "转发量",
-	comment: "评论量",
-	thumbs: "点赞量",
-}
+// const TRANSLATE = {
+// 	"﻿weiboname": "用户名",
+// 	weibotext: "微博",
+// 	topic: "话题",
+// 	date: "日期",
+// 	time: "时间",
+// 	forward: "转发量",
+// 	comment: "评论量",
+// 	thumbs: "点赞量",
+// }
+
+const IconText = ({ icon, text }) => (
+	<Space>
+		{React.createElement(icon)}
+		{text}
+	</Space>
+)
 
 export default observer(function Index() {
 	let { weiboContent } = store
-	weiboContent = weiboContent.length
-		? weiboContent
-		: Object.keys(TRANSLATE).map((d) => ({
-				type: d,
-				value: "",
-		  }))
+	// weiboContent = weiboContent.length
+	// 	? weiboContent
+	// 	: Object.keys(TRANSLATE).map((d) => ({
+	// 			type: d,
+	// 			value: "",
+	// 	  }))
 	return (
-        <Section title="微博详情">
-            <div className="content">
-                    {weiboContent.map((d) => (
-                        <div key={d.type} className="content-item">
-                            <span className="content-item-type">
-                                {TRANSLATE[d.type]}
-                            </span>
-                            <span className="content-item-value">{d.value}</span>
-                        </div>
-                    ))}
-            </div>
-        </Section>
+		<Section title="微博详情">
+			<div className="content">
+				<List
+					itemLayout="vertical"
+					size="large"
+					// pagination={{
+					// 	onChange: (page) => {
+					// 		console.log(page)
+					// 	},
+					// 	pageSize: 3,
+					// }}
+					dataSource={weiboContent}
+					renderItem={(item) => (
+						<List.Item
+							key={`${item["﻿weiboname"]}-${item.topic}-${item.date}-${item.time}`}
+							actions={[
+								<IconText
+									icon={ShareAltOutlined}
+									text={item.forward}
+									key="list-vertical-star-o"
+								/>,
+								<IconText
+									icon={LikeOutlined}
+									text={item.thumbs}
+									key="list-vertical-like-o"
+								/>,
+								<IconText
+									icon={MessageOutlined}
+									text={item.comment}
+									key="list-vertical-message"
+								/>,
+							]}
+						>
+							<List.Item.Meta
+								avatar={<Avatar icon={<UserOutlined />} />}
+								title={item["﻿weiboname"]}
+								description={item.topic}
+							/>
+							<Collapse
+								bordered={false}
+								expandIconPosition="right"
+							>
+								<Collapse.Panel
+									header={`${item.weibotext.slice(0, 15)} ...`}
+								>
+									{item.weibotext}
+								</Collapse.Panel>
+							</Collapse>
+						</List.Item>
+					)}
+				/>
+			</div>
+		</Section>
 	)
 })
